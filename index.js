35d52cb07864ff45cce70da5a9ce4be32d37e0ba
Pedro Tonini <pedro.tonini@nodis.com.br>
2020-11-17 15:17:00 -0300

    Updated dependencies

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const core = require('@actions/core');
-const exec = require('@actions/exec');
 const artifact = require('@actions/artifact');
 const fs = require('fs');
 const process = require('process');
@@ -8,17 +7,8 @@ const artifactClient = artifact.create();
 artifactClient.downloadArtifact('environmentVars').then(result => {
 
     const environmentVars = JSON.parse(fs.readFileSync('./environmentVars.json'));
-    for (let k in environmentVars) {
-        if (environmentVars.hasOwnProperty(k)) {
+    const stream = fs.createWriteStream(process.env.GITHUB_ENV, {flags:'a'});
 
-            let val = k + '=' + environmentVars[k] + '';
-
-            fs.appendFileSync(process.env.GITHUB_ENV, val);
-
-            // core.info(val);
-            // exec.exec("echo '" + val + "' >> $GITHUB_ENV")
-
-        } //core.exportVariable(k, environmentVars[k])
-    }
+    for (let k in environmentVars) environmentVars.hasOwnProperty(k) && stream.write(k + '=' + environmentVars[k] + '\n');
 
 }).catch(err => core.setFailed(err));
