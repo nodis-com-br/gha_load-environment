b1d1f0db24894a157dc77fc259b196cd8c3e0ce3
Pedro Tonini <pedro.tonini@nodis.com.br>
2020-11-17 11:02:35 -0300

    Updated env loading

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const core = require('@actions/core');
+const exec = require('@actions/exec');
 const artifact = require('@actions/artifact');
 const fs = require('fs');
 
@@ -8,7 +8,7 @@ artifactClient.downloadArtifact('environmentVars').then(result => {
 
     const environmentVars = JSON.parse(fs.readFileSync('./environmentVars.json'));
     for (let k in environmentVars) {
-        if (environmentVars.hasOwnProperty(k)) core.exportVariable(k, environmentVars[k])
+        if (environmentVars.hasOwnProperty(k)) exec.exec('echo "'+ k + '=' + environmentVars[k] + '" >> $GITHUB_ENV') //core.exportVariable(k, environmentVars[k])
     }
 
 }).catch(err => core.setFailed(err));
